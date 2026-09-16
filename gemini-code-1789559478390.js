cc70280de059dbdbc40f4861b72d3895e73a2393
tmphahlele010-lgtm <tmphahlele010@gmail.com>
2026-09-16 13:53:03 +0200

    Add files via upload

---
+++ gemini-code-1789559478390.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'panic-sos-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event - Serve cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
